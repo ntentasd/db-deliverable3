8a49ed6f6ecee712d24fd471a7f01a7860c3a5db
Dimitris-Ntentas <dimitrisntentas2002@gmail.com>
2025-01-03 02:37:00 +0200

    Restructuring

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql" // Import MySQL driver
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	// Retrieve jwt secret from the environment
 	jwtSecret := os.Getenv("JWT_SECRET")
 	if jwtSecret == "" {
 		log.Fatalf("JWT_SECRET environment variable is not set")
@@ -37,15 +38,25 @@ func main() {
 	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000, http://datadrive-ui",
-		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
-		AllowHeaders: "Content-Type, Authorization",
+		AllowOrigins:  "http://localhost:3000, http://datadrive-ui",
+		AllowMethods:  "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:  "Content-Type, Authorization",
+		ExposeHeaders: "Content-Length",
 	}))
 
+	app.Options("/*", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK) // Respond with 200 OK for preflight requests
+	})
+
 	// Setup routes
-	server := server.Server{Database: database}
-	server.SetupCarRoutes(app)
-	server.SetupTripRoutes(app, jwtSecret)
+	server := server.Server{
+		FiberApp:  app,
+		Database:  database,
+		JWTSecret: jwtSecret,
+	}
+	server.SetupCarRoutes()
+	server.SetupTripRoutes()
+	server.SetupUserRoutes()
 
 	// Start server
 	log.Fatal(app.Listen(":8000"))
