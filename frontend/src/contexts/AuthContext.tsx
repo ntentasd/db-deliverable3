19a75338f032a3f6d56c664fec67c093148d273e
Dimitris-Ntentas <dimitrisntentas2002@gmail.com>
2025-01-13 02:27:21 +0200

    AuthContext fixes

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -36,6 +36,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
   const logout = () => {
     setAuthToken(null);
+    window.location.href = "/";
   };
 
   useEffect(() => {
